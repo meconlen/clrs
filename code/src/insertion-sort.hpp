684910bae54438a4373e7a339e887a4688825b51
Mike Conlen <mike@conlen.org>
2025-04-12 17:28:50 -0400

    reverse iterator

--- code/src/insertion-sort.hpp
+++ code/src/insertion-sort.hpp
@@ -8,16 +8,16 @@ template<class T>
 void insertion_sort(T begin, T end)
 {
     if(begin == end) return;
-    T current = begin;
-    ++current;
-    while(current < end) {
+    auto rend = std::make_reverse_iterator(begin) - 1;
+    for(auto current = begin + 1; current < end; current++) {
         auto key = *current;
-        auto j = current;
-        while(j > begin && *(j-1) > key) {
-            *j = *(j+1);
-            --j;
+        auto j = std::make_reverse_iterator(current);
+        // NB: we are working with reverse iterators so we can go one past the beginning
+        //     but this means our index arithmetic is inverted; swaping '-' and '+' operators
+        while(j <=rend && *j > key) {
+            *(j-1) = *j;
+            ++j;
         }
-        *j = key;
-        current++;
+        *(j-1) = key;
     }
 }
